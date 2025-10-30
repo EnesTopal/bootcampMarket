f31a613495e5a715bad02b42f95e63f2d7d09154
EnesTopal <enes.topal1@gazi.edu.tr>
2025-10-30 18:01:46 +0300

    hide system ui

--- app/src/main/java/com/example/bootcampmarket/MainActivity.kt
+++ app/src/main/java/com/example/bootcampmarket/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.bootcampmarket
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.bootcampmarket.ui.theme.BootcampMarketTheme
 import com.example.bootcampmarket.ui.viewmodels.CartViewModel
 import com.example.bootcampmarket.ui.viewmodels.DetailViewModel
@@ -30,6 +35,7 @@ class MainActivity : ComponentActivity() {
     val profilesViewModel: ProfilesViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        hideSystemUI(window)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
@@ -37,6 +43,16 @@ class MainActivity : ComponentActivity() {
                 AppNavigation(mainViewModel,  cartViewModel, detailViewModel, favoritiesViewModel, profilesViewModel)
 
             }
+        }
+    }
+
+    @SuppressLint("InlinedApi", "WrongConstant")
+    fun hideSystemUI(window: Window) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowCompat.getInsetsController(window, window.decorView)
+        controller?.let {
+            it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            it.hide(WindowInsets.Type.navigationBars() or WindowInsets.Type.statusBars())
         }
     }
 }
